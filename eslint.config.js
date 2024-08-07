7c509b97423ae97b3ce6813cf696c9bda13609a5
MRH <mohammadreza.hayati1375@gmail.com>
2024-08-07 10:32:55 +0330

    base structure added

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+  ...tseslint.configs.recommended,
+];
